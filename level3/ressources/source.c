ebe762a2b966e5eabfd3f4b787863a04cffd8376
noctuelles <plouvel@student.42.fr>
2025-02-19 16:43:04 +0100

    feat(level3): source code

---
+++ level3/ressources/source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+static int m = 0;
+
+static int v()
+{
+  int result;
+  char s[520];
+
+  fgets(s, 512, stdin);
+  printf(s);
+  result = m;
+  if ( m == 64 )
+  {
+    fwrite("Wait what?!\n", 1u, 0xCu, stdout);
+    return system("/bin/sh");
+  }
+  return result;
+}
+
+int main(void)
+{
+  return v();
+}
